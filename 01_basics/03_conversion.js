43fbe830f7c4ee1041887e82410db19316a243b7
Shashank Ashlok <shashankashlok07@gmail.com>
2024-11-04 14:30:06 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,26 @@
+// let score = null
+
+// console.log(typeof score )
+// console.log(typeof(score))
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// 33 => number and valueInNumber = 33
+// "33"=> string and valueInNumber = 33
+//"33abc"=>string and valueInNumber = NaN(NOT A NUMBER )
+//null=>object and valueInNumber => 0 
+// let isLoggedIn = "shashank"
+// let bolleanLoggedIn = Boolean(isLoggedIn)
+// console.log(bolleanLoggedIn);
+//1=>true
+//0=>false
+//""=> false
+//"shashank"=>true 
+let a = 5
+let stringA = String(a)
+console.log(typeof stringA);
+console.log( stringA);
+//browser gives value in string 
+
+
+
